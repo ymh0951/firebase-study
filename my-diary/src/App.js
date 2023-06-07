d15a608d3b6ca037c36b1d98c0cd6dec05eb2068
Minho Yoo <ymh0951@naver.com>
2023-06-07 22:00:26 +0900

    Firebase 공부

---
+++ my-diary/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup';
+import Nav from './components/Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
---
+++ my-diary/src/components/Nav.js
@@ -0,0 +1,17 @@
+import styles from './Nav.module.css'
+import { Link } from 'react-router-dom'
+
+export default function Nav() {
+    return (
+      <nav className={styles.nav}>
+        <h1 className={styles.tit}>두근 두근 비밀일기</h1>
+        <ul className={styles.list_nav}>
+          <li>
+            <Link to="/login">로그인</Link></li>
+          <li>
+            <Link to="/signup">가입하기</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+}
