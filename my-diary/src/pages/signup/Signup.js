8bde8503e2cf233352f5d044099b6b2bd53725d2
Minho Yoo <ymh0951@naver.com>
2023-06-07 22:27:25 +0900

    env 파일 빼기

--- my-diary/src/pages/signup/Signup.js
+++ my-diary/src/pages/signup/Signup.js
@@ -1,5 +1,61 @@
+import { useState } from "react";
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleData = (event) => {
+    if (event.target.type === 'email') {
+      setEmail(event.target.value)
+    } else if (event.target.type === 'password') {
+      setPassword(event.target.value)
+    } else if (event.target.type === 'text') {
+      setDisplayName(event.target.value)
+    }
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password)
+  }
+
+  return (
+    <form className={styles.signup_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>회원가입</legend>
+        <label htmlFor="myEmail">email : </label>
+        <input
+          type="email"
+          id="myEmail"
+          required
+          value={email}
+          onChange={handleData}
+        />
+
+        <label htmlFor="myPassword">password : </label>
+        <input
+          type="password"
+          id="myPassword"
+          required
+          value={password}
+          onChange={handleData}
+        />
+
+        <label htmlFor="myNickName">닉네임 : </label>
+        <input
+          type="text"
+          id="myNickName"
+          required
+          value={displayName}
+          onChange={handleData}
+        />
+
+        <button type="submit" className={styles.btn}>
+          회원가입
+        </button>
+      </fieldset>
+    </form>
+  );
 }
